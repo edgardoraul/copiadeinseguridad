3214ea3b19f635dddc24266af9261c892a69893f
Edgard_Roul <contacto@webmoderna.com.ar>
2025-12-26 17:58:57 -0300

    Quitamos información sensible

--- config.bat
+++ config.bat
@@ -9,9 +9,4 @@ set OPCIONES=/E /Z /R:2 /W:2 /XJ /COPY:DAT /XF "NTUSER*" "desktop.ini" /XD "AppD
 
 :: Email (BLAT con SSL/TLS en puerto 465)
 set BLAT=blat.exe
-set SMTP_SERVER=mail.rerda.com
-set SMTP_PORT=587
-set MAIL_FROM=admin@rerda.com
-set MAIL_TO=edgardorerdamza@hotmail.com
-set MAIL_USER=admin@rerda.com
-set MAIL_PASS=Mprerda2026.
+call ..\datos.bat
