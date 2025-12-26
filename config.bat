7e0276235aa522bf6735a1ebdecf0258f75b3f2c
Edgard_Roul <contacto@webmoderna.com.ar>
2025-12-26 18:00:29 -0300

    Ofuscando.

--- config.bat
+++ config.bat
@@ -7,6 +7,5 @@ set TAR=bsdtar.exe
 :: Robocopy con progreso visible
 set OPCIONES=/E /Z /R:2 /W:2 /XJ /COPY:DAT /XF "NTUSER*" "desktop.ini" /XD "AppData"
 
-:: Email (BLAT con SSL/TLS en puerto 465)
-set BLAT=blat.exe
+:: Email envío con.... ¿qué carajos te importa, pelotudo?
 call ..\datos.bat
